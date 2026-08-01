72b8eddcd85fc30ebf074024351d64e55c61ca91
kimbafox <juegocrisger@gmail.com>
2026-08-01 01:17:49 -0400

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const server = require("./lex-bolivia-backend/server");
-
-const PORT = process.env.PORT || 4000;
-
-if (!server.listening) {
-  server.listen(PORT, () => {
-    console.log(`Yatilex backend escuchando en http://0.0.0.0:${PORT}`);
-  });
-}
